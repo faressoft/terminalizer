ecbaf11ea07069ea36ffd482d4d293c89db75331
Mohammad Fares <faressoft.com@gmail.com>
2018-10-01 12:25:56 +0300

    Remove extra method  from di

--- di.js
+++ di.js
@@ -4,31 +4,6 @@
  * @author Mohammad Fares <faressoft.com@gmail.com>
  */
 
-var is = require('is_js');
-
-/**
- * Dependencies
- * @type {Object}
- */
-var dependency = {};
-
-/**
- * Get a specific dependency
- * 
- * @param  {String}      key
- * @return {Object|Null} return null if not found
- */
-module.exports.get = function(key) {
-
-  // Not found
-  if (is.not.propertyDefined(dependency, key)) {
-    return null;
-  }
-
-  return dependency[key];
-  
-};
-
 /**
  * Set/Add a dependency
  * 
@@ -37,7 +12,6 @@ module.exports.get = function(key) {
  */
 module.exports.set = function(key, value) {
 
-  dependency[key] = value;
   module.exports[key] = value;
 
 };
